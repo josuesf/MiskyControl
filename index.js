e0854b1350020f7db1253c3503ed71fc2ab3bc02
josuesf <113552@unsaac.edu.pe>
2018-05-09 13:49:29 -0500

    background white drwin

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ app.on('ready', function () {
   //const { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize;
 
   mainWindow = new BrowserWindow({
-    width: 1200,//width,
-    height: 800,//height,
-    minHeight: 800,
-    minWidth: 1200,
+    width: 1100,//width,
+    height: 600,//height,
+    minHeight: 600,
+    minWidth: 1100,
     // maxHeight:800,
     // maxWidth:800,
-    frame: true,
+    frame: false,
   });
 
-  //mainWindow.setMenu(null);
+  mainWindow.setMenu(null);
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
@@ -33,5 +33,5 @@ app.on('ready', function () {
     mainWindow = null;
   });
 
-  mainWindow.toggleDevTools()
+  //mainWindow.toggleDevTools()
 });
